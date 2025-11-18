a2f71814e399be788c1982b4508ec4e9e5fc95dd
naktan02 <jmgjmg102@gmail.com>
2025-11-18 11:25:43 +0900

    1967

--- Baekjoon/1967.py
+++ Baekjoon/1967.py
@@ -1,20 +1,25 @@
 import sys
+sys.setrecursionlimit(20000)
 input = sys.stdin.readline
-n = input()
+n = int(input())
 tree = [[] for _ in range(n + 1)]
-for _ in range(n):
+for _ in range(n-1):
     a,b,c = map(int,input().split())
     tree[a].append((b,c))
 
 def dfs(start):
     if not tree[start]:
-        return 0
+        return (0,0)
     max_solo = 0
     max_group = 0
-    for node,weight in tree[start]:
-        plus = dfs(node)
-        if max_solo < weight + plus:
-            max_solo = weight + plus
-        elif max_solo + plus + weight > max_group:
-            max_group = max_solo + plus + weight
+    for now, cost in tree[start]:
+        a,b= dfs(now)
+        cost+=a
+        max_group = max(max_group,b,max_solo+cost)
+        max_solo = max(max_solo, cost)
+    return (max_solo, max_group)
+
+a,b = dfs(1)
+print(b)
         
+
