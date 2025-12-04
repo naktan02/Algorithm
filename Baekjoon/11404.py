4a77d2da7d040c0b76cae6893ef5b483a49ed346
naktan02 <jmgjmg102@gmail.com>
2025-12-04 19:17:23 +0900

    11404

---
+++ Baekjoon/11404.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+inf = float('inf')
+n = int(input())
+m = int(input())
+graph = [[inf] * (n + 1) for _ in range(n + 1)]
+
+for _ in range(m):
+    a,b,c = map(int,input().split())
+    graph[a][b] = min(graph[a][b],c)
+
+for i in range(1,n+1):
+    graph[i][i] = 0
+
+for k in range(1,n+1):
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            graph[i][j] = min(graph[i][j],graph[i][k] + graph[k][j])
+
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if graph[i][j] == inf:
+            print(0,end=' ')
+        else:
+            print(graph[i][j],end=' ')
+    print()
